0db07a1f7eb57be0a5fbba0ae69892238c51e9a9
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-08-27 17:20:41 -0300

    Semana 2: realicé la primer actividad 'SuperHeores' implementada en el proyecto Batallas

---
+++ Semana 2/Batallas/Batallas/SuperHeroe.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batallas
+{
+    internal class SuperHeroe
+    {
+        // Atributos
+        private string nombre;
+        private int fuerza;
+        private int resistencia;
+        private int superpoderes;
+
+        // Constructor
+        public SuperHeroe(string nombre, int fuerza, int resistencia, int superpoderes)
+        {
+            this.Nombre = nombre;
+            this.Fuerza = fuerza;
+            this.Resistencia = resistencia;
+            this.Superpoderes = superpoderes;
+        }
+
+        // Propiedades con validación de rango
+        public string Nombre
+        {
+            get { return nombre; }
+            private set { nombre = value; }
+        }
+
+        public int Fuerza
+        {
+            get { return fuerza; }
+            private set { fuerza = validarNumero(value); }
+        }
+
+        public int Resistencia
+        {
+            get { return resistencia; }
+            private set { resistencia = validarNumero(value); }
+        }
+
+        public int Superpoderes
+        {
+            get { return superpoderes; }
+            private set { superpoderes = validarNumero(value); }
+        }
+
+        // Método para validar que los atributos numéricos estén dentro del rango (0 a 100)
+        private int validarNumero(int value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 100)
+                return 100;
+            return value;
+        }
+
+        // Método para comparar los poderes de dos superhéroes
+        public string competir(SuperHeroe otroSuperheroe)
+        {
+            string resultado = "EMPATE";
+            int misPuntos = 0;
+            int susPuntos = 0;
+
+            if (this.Fuerza > otroSuperheroe.Fuerza)
+                misPuntos++;
+            else if (this.Fuerza < otroSuperheroe.Fuerza)
+                susPuntos++;
+
+            if (this.Resistencia > otroSuperheroe.Resistencia)
+                misPuntos++;
+            else if (this.Resistencia < otroSuperheroe.Resistencia)
+                susPuntos++;
+
+            if (this.Superpoderes > otroSuperheroe.Superpoderes)
+                misPuntos++;
+            else if (this.Superpoderes < otroSuperheroe.Superpoderes)
+                susPuntos++;
+
+            if (susPuntos >= 2)
+                resultado = "El ganador es " + otroSuperheroe.Nombre;
+            else if (misPuntos >= 2)
+                resultado = "El ganador es " + this.Nombre;
+            return resultado;
+        }
+
+    }
+}
---
+++ Semana 2/Batallas/Batallas/Test.cs
@@ -0,0 +1,31 @@
+﻿using Batallas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Batallas
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            // Crear los superhéroes según el enunciado
+            SuperHeroe superHeroe1 = new SuperHeroe("Batman", 98, 70, 0);
+            SuperHeroe superHeroe2 = new SuperHeroe("Superman", 95, 60, 70);
+            SuperHeroe superHeroe3 = new SuperHeroe("Gohan", 100, 100, 70);
+
+            // Jugar y mostrar resultados
+            // Aca compite batman contra superman 
+            string resultado1 = superHeroe1.competir(superHeroe2);
+            // Aca compite superman contra Gohan
+            string resultado2 = superHeroe2.competir(superHeroe3);
+
+            Console.WriteLine("Resultado de Batman vs. Superman: " + resultado1);
+            Console.WriteLine("Resultado de Superman vs. Gohan: " + resultado2);
+        }
+    }
+}
+ 
