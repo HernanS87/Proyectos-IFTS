999df70603899250e4920784f26c7b4f5053c27b
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-08-30 20:59:04 -0300

    Practiqué la construccion rapida de los getter, setter y constructores

---
+++ Semana 2/IngresandoDatos/IngresandoDatos/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngresandoDatos
+{
+    internal class Test
+    {
+         private String nombre;
+         private int dni;
+
+        public Test()
+        {
+        }
+
+        public Test(string nombre, int dni)
+        {
+            this.nombre = nombre ?? throw new ArgumentNullException(nameof(nombre));
+            this.dni = dni;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Dni { get => dni; set => dni = value; }
+    }
+}
